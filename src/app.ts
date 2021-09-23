52a272a1358e74e6e4659f25c41c79db074470c6
tnsgud9 <tnsgud9@naver.com>
2021-09-23 21:19:46 +0900

    add router setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("hello world!");
+import express from "express";
+import { createServer, Server } from "http";
+
+import controller from "./controller";
+
+const app = express();
+
+app.use(controller);
+
+const server = createServer(app);
+server.listen(process.env.PORT || 5000);
